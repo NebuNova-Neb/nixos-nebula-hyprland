a4a3cc60fc41c29e75445a0de373f2e4889cf492
NebuNova-Neb <nebu.seiun@gmail.com>
2026-09-22 13:49:20 -0700

    submaster 1.0

---
+++ home.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "nebula";
+  home.homeDirectory = "/home/nebula";
+  home.stateVersion = "26.05";
+   
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;  
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    fastfetch
+    starship
+    htop
+    vlc
+    libreoffice-fresh
+    gimp
+    tree
+    discord
+    zsh
+    zsh-autosuggestions
+    zsh-autocomplete
+  ];
+ 
+programs.starship = {
+  enable = true;
+};
+         
+   imports = [
+   ~/.config/home-manager/fastfetch.nix
+   ~/.config/home-manager/zsh.nix
+   ~/.config/home-manager/ghostty.nix
+      
+  ]; 
+
+
+}
+
